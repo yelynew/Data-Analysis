8a8ee5e271b4b4a86b70d47fb3532ef12a04a77c
yelynew <54059519+yelynew@users.noreply.github.com>
2021-07-15 13:21:25 +0900

    Create Kkma_Komoran_test.py
    Checking Kkma and Komoran library

---
+++ Kkma_Komoran_test.py
@@ -0,0 +1,7 @@
+from konlpy.tag import Kkma 
+kkma = Kkma()
+print(kkma.morphs(u'꼬꼬마 잘 나오는지 확인하기.'))
+
+from konlpy.tag import Komoran 
+komoran = Komoran() 
+print(komoran.morphs(u'우왕 코모란도 오픈소스가 되었어요'))
