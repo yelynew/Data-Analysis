8f93d27d2db0c541ef0e59d1c54421e495003b23
yelynew <54059519+yelynew@users.noreply.github.com>
2021-07-15 13:12:19 +0900

    Create extract unique differences without english.py
    You can extract unique differences between two columns without English (only Korean)
    엑셀의 두개의 열을 각각 비교하여 다른 값을 엑셀로 추출 가능 (중복 제거, 한글 외 영문 제거)

---
+++ extract unique differences without english.py
@@ -0,0 +1,68 @@
+import io
+import re
+import pandas as pd
+
+df = pd.read_excel('your address')
+tag=df['tags']
+#print(type(tag))
+tag.head()
+token=df['token']
+token.head()
+
+tags=[]
+for i in tag:
+  i=str(i)
+  i = re.sub("\[|\]|\'| ","",i)
+  i = re.sub("\#",",",i)
+  hangul = re.compile('[^ ㄱ-ㅣ가-힣]+,') # 한글과 띄어쓰기를 제외한 모든 글자
+  i = hangul.sub('', i) # 한글과 띄어쓰기를 제외한 모든 부분을 제거
+  ntagi = i.split(",")
+  tags.append(ntagi)
+print(tags) 
+  #print(ntagi[0])
+
+tokens=[]
+for j in token:
+  j=str(j)
+  j = re.sub("\[|\]|\'| ","",j)
+  #print(j)
+  ntokenj = j.split(",")
+  #print(ntokenj)
+  tokens.append(ntokenj)
+print(tokens)
+diff1=[]
+diff2=[]
+for x in range(len(tags)):
+    y=tags[x]
+    z=tokens[x]
+    y1=tags[x]
+    z1=tokens[x]
+    for k in y:
+      for l in z:
+        if (k==l):
+          if (k in y1):
+            y1.remove(k)
+          if (l in z1):
+            z1.remove(l)
+
+    set1=set(y1)
+    list1=list(set1)
+    set2=set(z1)
+    list2=list(set2)
+    diff1.append(list1)
+    diff2.append(list2)
+
+
+
+#print(diff1)
+#print(diff2)
+
+import pandas as pd
+import numpy as np
+
+df=pd.DataFrame.from_records(diff1)
+df.to_excel('diff3.xlsx')
+df2=pd.DataFrame.from_records(diff2)
+df2.to_excel('diff4.xlsx')
+
+#중복제거
